bfa0b7e9882b02fdf0f0de26a3cac768ff75faf9
wackshopr-tech <wack.shopr@gmail.com>
2026-09-12 14:14:40 +0700

    Update Main-GUI-2.lua

--- Main-GUI-2.lua
+++ Main-GUI-2.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://rizz-rxs.xyz/Onyx-Zero/api?key=07331f83389b3e8a7ed870bd649c3076&_s=cdvhtn"))()
+loadstring(game:HttpGet('https://rizz-rxs.xyz/Onyx-Zero/api.php?key=07331f83389b3e8a7ed870bd649c3076'))()
