d4fe96dfc1b1340a82593aa463fb3261ecc465ba
wackshopr-tech <wack.shopr@gmail.com>
2026-09-12 14:48:23 +0700

    Update script URL for loading GUI

--- Main-GUI-2.lua
+++ Main-GUI-2.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://rizz-rxs.xyz/Onyx-Zero/api.php?key=07331f83389b3e8a7ed870bd649c3076'))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/myks272626/RUNLUA-HUB/refs/heads/main/Gui-1-obfuscated.lua"))()
